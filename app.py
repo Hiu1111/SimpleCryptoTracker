805683d2e76467e3e331b63eb22f95c608e84c61
Hiu Cheung <112709119+Hiu1111@users.noreply.github.com>
2025-01-02 03:50:09 -0500

    app.py

--- app.py
+++ app.py
@@ -3,19 +3,20 @@ import requests
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') #Sets the homepage 
 def home():
     return "Welcome to the Cryptocurrency Portfolio Tracker!"
 
-@app.route('/crypto-prices', methods=['GET'])
+@app.route('/crypto-prices', methods=['GET']) #second page which brings you to the list of cryptoprices 
 def get_crypto_prices():
-    url = 'https://api.coingecko.com/api/v3/simple/price'
-    params = {'ids': 'bitcoin,ethereum', 'vs_currencies': 'usd'}
-    response = requests.get(url, params=params).json()
-    return render_template('index.html', prices=response)
+    url = 'https://api.coingecko.com/api/v3/simple/price' #fetches the data using coingecko api 
+    params = {'ids': 'bitcoin,ethereum', 'vs_currencies': 'usd'} #shows which coins are being fetched 
+    response = requests.get(url, params=params).json() #makes a get request to get the data 
+    return render_template('index.html', prices=response) #passes the data over to html to display the data passed to it
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
     
 
